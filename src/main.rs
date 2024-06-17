764a7ff232e1259ae2c0e57534893be3b804d130
Ivan Bodrov <ibodrov@gmail.com>
2024-06-16 22:57:00 -0400

    initial

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use async_trait::async_trait;
+use ezsockets::ClientConfig;
+use std::{env, io::BufRead};
+
+struct Client {}
+
+#[async_trait]
+impl ezsockets::ClientExt for Client {
+    type Call = ();
+
+    async fn on_text(&mut self, text: String) -> Result<(), ezsockets::Error> {
+        tracing::info!("received message: {text}");
+        Ok(())
+    }
+
+    async fn on_binary(&mut self, bytes: Vec<u8>) -> Result<(), ezsockets::Error> {
+        tracing::info!("received bytes: {bytes:?}");
+        Ok(())
+    }
+
+    async fn on_call(&mut self, call: Self::Call) -> Result<(), ezsockets::Error> {
+        let () = call;
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let config = ClientConfig::new("ws://localhost:8001/websocket")
+        .header(http::header::AUTHORIZATION, env::var("API_KEY").unwrap());
+    let (handle, future) = ezsockets::connect(|_client| Client {}, config).await;
+    tokio::spawn(async move {
+        future.await.unwrap();
+    });
+    let stdin = std::io::stdin();
+    let lines = stdin.lock().lines();
+    for line in lines {
+        let line = line.unwrap();
+        tracing::info!("sending {line}");
+        handle.text(line).unwrap();
+    }
+}
