fa01dbadfe30640eb5e5f1f4eb4e8414bb50cfa6
Ivan Bodrov <ibodrov@gmail.com>
2024-07-30 21:39:48 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,70 @@
-use http::{Request, Uri};
-use tokio_tungstenite::connect_async;
-use tracing::info;
+use std::{env, sync::Arc, time::Duration};
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
+use concord_client::{Config, Message, QueueClient};
+use dotenvy::dotenv;
+use http::Uri;
+use tokio::{sync::Mutex, time::{interval, sleep}};
+use tracing::{error, info};
 
-    let api_key = std::env::var("SERVER_API_KEY").expect("SERVER_API_KEY must be set");
-    let uri = Uri::from_static("ws://localhost:8001/websocket");
-    let host = uri.host().expect("Invalid host in WebSocket URL");
-
-    let request = Request::builder()
-        .uri(&uri)
-        .header("Host", host)
-        .header("Authorization", api_key)
-        .header("Connection", "upgrade")
-        .header("Upgrade", "websocket")
-        .header("User-Agent", "concord-agent-rs")
-        .header("sec-websocket-key", "concord-agent-rs")
-        .header("sec-websocket-version", 13)
-        .body(())
-        .unwrap();
-    let (ws_session, _) = connect_async(request).await.unwrap();
-
-    info!("Connected to {}", uri);
+#[derive(Debug)]
+struct AppError {
+    message: String,
 }
 
+impl std::error::Error for AppError {}
+
+impl std::fmt::Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
+    dotenv().ok();
+
+    let uri = "ws://localhost:8001/websocket".parse::<Uri>()?;
+    info!("Connecting to {}", uri);
+
+    let auth_header = env::var("AUTHORIZATION_HEADER").map_err(|_| AppError {
+        message: "AUTHORIZATION_HEADER is not set".to_string(),
+    })?;
+
+    let config = Config { uri, auth_header };
+    let queue_client = Arc::new(Mutex::new(QueueClient::connect(config).await?));
+    info!("Connected to the server");
+
+    // ping the server every 30 seconds
+    let ping_interval = Duration::from_secs(10);
+    let ping_client = queue_client.clone();
+    tokio::spawn(async move {
+        loop {
+            sleep(ping_interval).await;
+            let mut queue_client = ping_client.lock().await;
+            if let Err(err) = queue_client.send_message(Message::Ping).await {
+                error!("Failed to send ping message: {}", err);
+            }
+            info!("Sent ping message");
+        }
+    });
+
+    loop {
+        info!("Waiting for messages");
+
+        let mut queue_client = queue_client.lock().await;
+        match queue_client.receive_message().await? {
+            Message::CommandResponse { correlation_id } => todo!(),
+            Message::ProcessResponse { correlation_id, session_token, process_id } => todo!(),
+            Message::Ping => {
+                queue_client.send_message(Message::Pong).await?;
+                info!("Sent pong message");
+            },
+            Message::Pong => {},
+            _ => {}
+        }
+
+        sleep(Duration::from_secs(5)).await;
+    }
+}
