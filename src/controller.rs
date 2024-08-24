075ccff063fc0a898ece75de2b2cea72c7050ebd
Ivan Bodrov <ibodrov@gmail.com>
2024-08-23 20:06:48 -0400

    wip

---
+++ src/controller.rs
@@ -0,0 +1,14 @@
+use futures_util::TryFutureExt;
+use kube::Client;
+use tracing::info;
+
+use crate::{app_error, error::AppError};
+
+pub async fn run() -> Result<(), AppError> {
+    let client = Client::try_default()
+        .map_err(|e| app_error!("Can't connect to K8s: {e}"))
+        .await?;
+    info!("Connected to Kubernetes API...");
+
+    Ok(())
+}
